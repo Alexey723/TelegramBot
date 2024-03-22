524eb228c369f45824a05ae5b5742a377d927447
Alex <112931512+Alexey723@users.noreply.github.com>
2024-03-22 14:37:41 +0300

    Update MailParams.java

--- mail-service/src/main/java/ru/conditer/dto/MailParams.java
+++ mail-service/src/main/java/ru/conditer/dto/MailParams.java
@@ -6,7 +6,9 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@RequiredArgsConstructor
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MailParams {
     private String id;
     private String emailTo;
